563a639e4254c1e3d8d7e19f6fcea471981e0135
harteh <harteh31@gmail.com>
2019-11-06 17:48:42 +0900

    다시 업로드

---
+++ animal02.js
@@ -0,0 +1,52 @@
+// 메뉴 호버 설정 시작
+$(document).ready(function(){
+
+
+    $("#nav .nav_list .dep1").mouseover(function () {
+        $(".dep2", this).slideDown();
+    });
+    $("#nav .nav_list .dep1").mouseleave(function () {
+        $(".dep2", this).stop().slideUp();
+    });
+
+
+    
+});
+// 메뉴 호버 설정 끝
+
+// 베너 슬라이드 설정 시작
+$(document).ready(function(){
+    var imgs;
+    var img_count;
+    var img_position = 1;
+
+    imgs = $(".slide_wrap");
+    img_count = imgs.children().length;
+    
+    //버튼을 클릭했을 대 함수 실행
+    $('#back').click(function () {
+        back();
+    });
+    $('#next').click(function (){
+        next();
+    });
+
+    function back() {
+        if(1<img_position){
+            imgs.animate({
+                left:'+=1200px'
+            });
+            img_position--;
+        }
+    }
+    function next() {
+        if(img_count>img_position){
+            imgs.animate({
+                left:'-=1200px'
+            });
+            img_position++;
+        }
+    }
+
+})
+// 베너 슬라이드 설정 끝
